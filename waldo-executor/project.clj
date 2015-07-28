2d3edfc6b1307c6683deb6d3b366683ff822bf03
Alexander Maricich <w33tmaricich@gmail.com>
2015-07-28 09:08:50 -0400

    Moved over pre-written functionality
    > Incorporated spread library.
    > Modified the core to contain a logic function.
    > Included my pretty messaging functions.
    
    < A lot of what needs to be done at the moment has to do with ingesting
    < code from somewhere else, and running that code. I need to look into
    < what the best way is for doing just that.

--- waldo-executor/project.clj
+++ waldo-executor/project.clj
@@ -1,9 +1,10 @@
 (defproject waldo-executor "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "The waldo executor is an application that allows programs using the waldo library to begin execution after migrating from another system."
+  :url "FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :main ^:skip-aot waldo-executor.core
   :target-path "target/%s"
+  :resource-paths ["resources/spread.jar"]
   :profiles {:uberjar {:aot :all}})
--- waldo-executor/src/waldo_executor/core.clj
+++ waldo-executor/src/waldo_executor/core.clj
@@ -1,7 +1,13 @@
 (ns waldo-executor.core
+  (:require [waldo-executor.utils.messages :as msg])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
+(defn logic
+  "Holds the sequence of events that will take place in the executor."
   [& args]
-  (println "Hello, World!"))
+  (msg/message "nope" "Hello, World!" args))
+
+(defn -main
+  "Starts when the application launches."
+  [& args]
+  (apply logic args))
