954ab63e0bf45e3d81f58056820c644d13517b37
Alexander Maricich <w33tmaricich@gmail.com>
2015-07-29 14:46:42 -0400

    Created single thread code execution loop
    > Waits for code to be sent to the waldo-execute group.
    > Once the code reaches, the code will be executed and it will wait
    > again for code to reach it.

--- waldo-executor/src/waldo_executor/core.clj
+++ waldo-executor/src/waldo_executor/core.clj
@@ -4,36 +4,30 @@
             [waldo-executor.utils.messages :as msg])
   (:gen-class))
 
+(def retrieve-group "waldo-execute")
+(def spread-con {:ip "127.0.0.1"
+                 :port 4803
+                 :private-name "waldo-executor"
+                 :priority false
+                 :group-membership false})
+
 (defn logic
   "Holds the sequence of events that will take place in the executor."
   [& args]
-  (let [connection (spread/connect (spread/connection-information "127.0.0.1" 4803 "waldo-executor" false false))
-        grp-retrieve (spread/join-group "retrieve" connection)]
-    (spread/push connection grp-retrieve "(+ 1 1)")
-    (def code (spread/pull connection))
-    (def code-list (string->list code))
-    (execute-list code-list)
-
-    (spread/disconnect connection)))
-
-
-
-  ;(let [code-list (file->list "/tmp/test.clj")
-        ;info (hand-briefcase code-list)
-        ;config (:config info)
-        ;data (:data info)
-        ;fmap (:fmap info)
-        ;trail (:trail info)]
-
-    ;(msg/data 'info info)
-    ;(msg/data 'config config)
-    ;(msg/data 'data data)
-    ;(msg/data 'fmap fmap)
-    ;(msg/data 'trail trail)
-
-    ;(execute-list code-list)
-    ;((:do-next fmap))))
-
+  (let [connection (spread/connect (spread/connection-information (:ip spread-con)
+                                                                  (:port spread-con)
+                                                                  (:private-name spread-con)
+                                                                  (:priority spread-con)
+                                                                  (:group-membership spread-con)))
+        grp-retrieve (spread/join-group retrieve-group connection)]
+    (msg/suc "Waiting for code...")
+    (loop [code (spread/pull connection)] ; Retrieve code to be run from spread
+      (try
+        (execute-list (string->list code))
+        (catch Exception e (do
+                             (msg/err (str "Unable to execute code: " (.getMessage e)))
+                             (spread/disconnect connection))))
+      (recur (spread/pull connection)))))
 
 (defn -main
   "Starts when the application launches."
