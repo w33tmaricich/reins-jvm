ee2830181d545399339252939fbe317a092d9c50
Alexander Maricich <w33tmaricich@gmail.com>
2015-08-04 15:45:59 -0400

    Added first iteration of simple chat client
    > Created a simple chat client in clojure.

---
+++ agentchat.clj
@@ -0,0 +1,40 @@
+;;;; Agent Chat
+;;;;
+;;;; This is a super simple chat client implemented in a mobile agent.
+
+{:config {:communication-method "TCP"
+          :port 8002}
+ :data {:current-location "192.168.1.7"
+        :partner-location "192.168.1.18"
+        :message "Welcome to agent chat!\n======================\nStart typing and press enter to send your message!\n"}
+ :do-next "chat"}
+
+(import java.net.Socket)
+(import java.io.DataOutputStream)
+(import java.io.BufferedOutputStream)
+
+(defn move
+  "Jumps to another executor"
+  [ip port briefcase]
+  (try
+    (let [socket (Socket. ip port)
+          out-stream (DataOutputStream. (BufferedOutputStream. (.getOutputStream socket)))]
+      (.writeUTF out-stream (str briefcase))
+      (.flush out-stream)
+      (.close out-stream)
+      (println " [ suc ] --> Move to" ip ":" port "was successful."))
+    (catch Exception e (println "Error: " e))))
+
+(defn chat
+  "This function will run when the agent reaches its destination."
+  [briefcase]
+  (println (:message (:data briefcase)))
+  (move (:partner-location (:data briefcase))
+        (:port (:config briefcase))
+        {:config (:config briefcase)
+         :do-next "((declare chat))"
+         :data {:current-location (:partner-location (:data briefcase))
+                :partner-location (:current-location (:data briefcase))
+                :message (str (read-line))}
+         :code (:code briefcase)}))
+  ;(move "127.0.0.1" (:port (:config briefcase)) briefcase))
